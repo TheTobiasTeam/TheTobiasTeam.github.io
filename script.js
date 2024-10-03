955db0bf10dd3fe492dfb91849d925fe9a7e3274
jonahlikescookies <jonah.cookie77@gmail.com>
2024-10-02 20:16:37 -0700

    revamp website

--- script.js
+++ script.js
@@ -1,29 +1,24 @@
-// Wait for the document to load before running the script 
-(function ($) {
-  
-  // We use some Javascript and the URL #fragment to hide/show different parts of the page
-  // https://developer.mozilla.org/en-US/docs/Web/HTML/Element/a#Linking_to_an_element_on_the_same_page
-  $(window).on('load hashchange', function(){
-    
-    // First hide all content regions, then show the content-region specified in the URL hash 
-    // (or if no hash URL is found, default to first menu item)
-    $('.content-region').hide();
-    
-    // Remove any active classes on the main-menu
-    $('.main-menu a').removeClass('active');
-    var region = location.hash.toString() || $('.main-menu a:first').attr('href');
-    
-    // Now show the region specified in the URL hash
-    $(region).show();
-    
-    // Highlight the menu link associated with this region by adding the .active CSS class
-    $('.main-menu a[href="'+ region +'"]').addClass('active'); 
+function changeTab() {
+  let path = location.hash.toString();
+  if (path === "#") {
+    path = "#home";
+  }
+  path = path.slice(1);
+  for (const childDiv of document.querySelectorAll("main > div")) {
+    childDiv.style.display = "none";
+  }
+  const content = document.getElementById(path);
+  content.style.display = "block";
+  for (const link of document.querySelectorAll("nav > a")) {
+    link.classList.remove("active");
+  }
+  document.querySelector(`nav > a[href=\"#${path}\"]`).classList.add("active");
+}
 
-    // Alternate method: Use AJAX to load the contents of an external file into a div based on URL fragment
-    // This will extract the region name from URL hash, and then load [region].html into the main #content div
-    // var region = location.hash.toString() || '#first';
-    // $('#content').load(region.slice(1) + '.html')
-    
-  });
-  
-})(jQuery);
+window.addEventListener("DOMContentLoaded", () => {
+  changeTab();
+});
+
+window.addEventListener("hashchange", () => {
+  changeTab();
+});
