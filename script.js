6362dd1990d051c6ef6f301a72120e0138ec6ad6
jonahlikescookies <jonah.cookie77@gmail.com>
2024-10-03 17:31:27 -0700

    light/dark theme and starting on improved bookmarklet system

--- script.js
+++ script.js
@@ -4,15 +4,19 @@ function changeTab() {
     path = "#home";
   }
   path = path.slice(1);
-  for (const childDiv of document.querySelectorAll("main > div")) {
-    childDiv.style.display = "none";
+  if (["home", "bookmarklets", "proxies"].includes(path)) {
+    for (const childDiv of document.querySelectorAll("main > div")) {
+      childDiv.style.display = "none";
+    }
+    const content = document.getElementById(path);
+    content.style.display = "block";
+    for (const link of document.querySelectorAll("nav > a")) {
+      link.classList.remove("active");
+    }
+    document.querySelector(`nav > a[href=\"#${path}\"]`).classList.add("active");
+  } else {
+    // do stuff for jumping to bookmarklets
   }
-  const content = document.getElementById(path);
-  content.style.display = "block";
-  for (const link of document.querySelectorAll("nav > a")) {
-    link.classList.remove("active");
-  }
-  document.querySelector(`nav > a[href=\"#${path}\"]`).classList.add("active");
 }
 
 window.addEventListener("DOMContentLoaded", () => {
